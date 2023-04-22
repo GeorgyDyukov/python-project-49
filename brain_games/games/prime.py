c20ca246c754f0eb798f68b1860cf4f84fd74795
Georgy Dyukov <georgy.dyukov@gmail.com>
2023-04-22 09:20:21 +0300

    Create prime game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,34 @@
+import random
+game_description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def task():
+    '''
+    The function generates a random integer and a string indicating whether the generated number is prime or not.
+
+    Returns:
+        question (int): a randomly generated integer between 0 and 10.
+        correct_answer (str): a string indicating whether the generated number is prime ('yes') or not ('no').
+    '''
+    question = random.randint(0, 10)
+    correct_answer = is_prime(question)
+    return question, correct_answer
+
+
+def is_prime(question):
+    '''
+    The function returns 'yes' if the entered number is prime.
+    Otherwise the function returns 'no'.
+
+    Args:
+        number (int): The number to check.
+
+    Returns:
+        str: 'yes' if the number is prime, 'no' if not.
+    '''
+    if question <= 1:
+        return 'no'
+    for i in range(2, int(question ** 0.5) + 1):
+        if question % i == 0:
+            return 'no'
+    return 'yes'
