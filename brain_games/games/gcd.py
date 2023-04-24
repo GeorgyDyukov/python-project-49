f330b2eb41c2bb4ebc91de7b8a33029f42fadc3d
Georgy Dyukov <georgy.dyukov@gmail.com>
2023-04-24 17:55:04 +0300

    Fix E501 errors in gcd game

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,11 +5,14 @@ game_description = 'Find the greatest common divisor of given numbers.'
 
 def task():
     '''
-    The function generates a random math expression in the form of two randomly generated integers and calculates their greatest common divisor (GCD).
+    The function generates a random math expression in the form of two randomly
+    generated integers and calculates their greatest common divisor (GCD).
 
     Returns:
-        question (str): a string representing the randomly generated math expression in the form of 'a b'.
-        correct_answer (str): a string representing the greatest common divisor of the two randomly generated integers.
+        question (str): a string representing the randomly generated
+            math expression in the form of 'a b'.
+        correct_answer (str): a string representing the greatest common divisor
+            of the two randomly generated integers.
     '''
     a = random.randint(-10, 10)
     b = random.randint(-10, 10)
@@ -20,13 +23,14 @@ def task():
 
 def find_gcd(a, b):
     '''
-    The function takes two integers and finds their greatest common divisor (GCD).
+    The function takes two integers and finds their greatest common divisor.
 
     Parameters:
         a (int): A random number between -10 and 10.
         b (int): A random number between -10 and 10.
 
     Returns:
-        str: a string representing the greatest common divisor of the two input integers.
+        str: a string representing the greatest common divisor
+            of the two input integers.
     '''
     return str(gcd(a, b))
