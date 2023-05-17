33c7ee97ad3505aa98ed5ff4a07c7486e06180b2
Georgy Dyukov <georgy.dyukov@gmail.com>
2023-05-17 10:53:23 +0300

    Fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,5 +1,7 @@
 import random
-game_description = 'Answer "yes" if the number is even, otherwise answer "no".'
+GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+MIN_NUMBER = -10
+MAX_NUMBER = 10
 
 
 def task():
@@ -8,26 +10,25 @@ def task():
     indicating whether the generated number is even or odd.
 
     Returns:
-        question (int): a randomly generated integer between -10 and 10.
-        correct_answer (str): a string indicating whether the generated number
-            is even ('yes') or odd ('no').
+        question (int): a randomly generated integer
+            within a specific numerical range.
+        correct_answer (str): a string indicating whether
+            the generated number is even ('yes') or odd ('no').
     '''
-    question = random.randint(-10, 10)
-    correct_answer = is_even(question)
+    question = random.randint(MIN_NUMBER, MAX_NUMBER)
+    correct_answer = 'yes' if is_even(question) else 'no'
     return question, correct_answer
 
 
 def is_even(question):
     '''
-    The function returns 'yes' if the entered number is even,
-    and 'no' if the entered number is odd.
+    The function returns True if the entered number is even
+    and False if the entered number is odd.
 
     Args:
         question (int): The number to check.
 
     Returns:
-        str: 'yes' if the number is even, 'no' if the number is odd.
+        bool: True if the number is even, False if the number is odd.
     '''
-    if question % 2 == 0:
-        return 'yes'
-    return 'no'
+    return True if question % 2 == 0 else False
