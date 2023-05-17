9ae624294cce540fd39a3f1ca9c7d6b2a0c3314b
Georgy Dyukov <georgy.dyukov@gmail.com>
2023-05-17 10:54:19 +0300

    Fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,9 +1,15 @@
 import random
-game_description = 'What number is missing in the progression?'
+GAME_DESCRIPTION = 'What number is missing in the progression?'
+MIN_INITIAL_TERM = -10
+MAX_INITIAL_TERM = 10
+MIN_COMMON_DIFF = 1
+MAX_COMMON_DIFF = 5
+MIN_MEMBERS_AMOUNT = 5
+MAX_MEMBERS_AMOUNT = 10
 
 
 def task():
-    ''''
+    '''
     The function generates an arithmetic progression.
     Then it selects one member of the progression at random
     and replaces its value with a placeholder.
@@ -14,14 +20,31 @@ def task():
     correct_answer (str): a string representing the value
         of the missing progression member.
     '''
-    progression_start = random.randint(-10, 10)
-    progression_members_amount = random.randint(5, 10)
-    progression_step = random.randint(1, 5)
-    progression = []
-    for i in range(progression_members_amount):
-        progression.append(str(progression_start + progression_step * i))
-    question_index = random.randint(0, progression_members_amount - 1)
+    initial_term = random.randint(MIN_INITIAL_TERM, MAX_INITIAL_TERM)
+    common_diff = random.randint(MIN_COMMON_DIFF, MAX_COMMON_DIFF)
+    members_num = random.randint(MIN_MEMBERS_AMOUNT, MAX_MEMBERS_AMOUNT)
+    progression = generate_progression(initial_term, common_diff, members_num)
+    question_index = random.randint(members_num - members_num, members_num - 1)
     correct_answer = progression[question_index]
     progression[question_index] = '..'
     question = ' '.join(progression)
     return question, correct_answer
+
+
+def generate_progression(initial_term, common_diff, members_num):
+    '''
+    The function generates an arithmetic progression based on
+    the given initial term, common difference, and number of members.
+
+    Args:
+        initial_term (int): The first term of the arithmetic progression.
+        common_diff (int): The common difference between consecutive terms.
+        members_amount (int): The number of members in the progression.
+
+    Returns:
+        progression (list): A list containing the generated progression.
+    '''
+    progression = []
+    for i in range(members_num):
+        progression.append(str(initial_term + common_diff * i))
+    return progression
